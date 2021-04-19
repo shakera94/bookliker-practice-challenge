e0bfea1277735541fe8122cac0caab2ee6b20b81
shakera <sanderso_4516574@yahoo.com>
2021-04-18 23:54:50 -0400

    completed assignment

--- js/index.js
+++ js/index.js
@@ -1,1 +1,19 @@
-document.addEventListener("DOMContentLoaded", function() {});
+document.addEventListener("DOMContentLoaded", function() {
+getBooks() .then(renderBooks)});
+
+function getBooks(){
+    fetch('http://localhost:3000/books')
+    .then(resp => resp.json())
+  .then(books => console.log(json))
+}
+
+function renderBooks() {
+  const list = document.getElementById('list-panel');
+  books.forEach(book => {
+    const ul = document.getElementById('ul');
+    ul.innerHTML = book.name;
+    list.appendChild(ul);
+  });
+}
+
+
